fee97fbc5ad108985d96b9dd43a6624b619d16f3
yong <tkddbs1644@gmail.com>
2022-11-02 16:50:29 +0900

    Feat: car api 기능 구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,15 @@
+//import carApi from "./apis/car";
+
 function App() {
-  return <h1>Hello React!</h1>;
+  const handleClick = async () => {
+    //const res = await carApi.getCars();
+    //console.log(res);
+  };
+  return (
+    <button type="button" onClick={handleClick}>
+      모든 차 불러오기
+    </button>
+  );
 }
 
 export default App;
