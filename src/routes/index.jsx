4a8e4e1e1522649adc5a8df142123023fd3ec815
chaechae <dokailove6326@gmail.com>
2022-11-02 13:11:25 +0900

    Feat: Router 지정

---
+++ src/routes/index.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { BrowserRouter, Route, Routes } from "../../node_modules/react-router-dom/dist/index";
+import CarList from "@/pages/CarList/index";
+import { ROUTER_PATH } from "./rotuerPath";
+import CarDetail from "@/pages/CarDetail/index";
+import NotFound from "@/pages/NotFound/index";
+
+function Router() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route index path={ROUTER_PATH.BASE} element={<CarList />} />
+        <Route index path={ROUTER_PATH.DETAIL} element={<CarDetail />} />
+        <Route index path={ROUTER_PATH.NOTFOUND} element={<NotFound />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default Router;
---
+++ src/routes/rotuerPath.js
@@ -0,0 +1,5 @@
+export const ROUTER_PATH = {
+  BASE: "/",
+  DETAIL: "/detail",
+  NOTFOUND: "*",
+};
