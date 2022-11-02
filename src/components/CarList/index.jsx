f902ee1989ccc2b25efc92c26f4d747843eba45d
chaechae <dokailove6326@gmail.com>
2022-11-02 23:44:22 +0900

    Rename: CarList 추가

---
+++ src/components/CarList/index.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { Container } from "./CarList.styled";
+
+function CarList() {
+  // const { handleCarData } = useContext(CarListContext);
+  // console.log("res", handleCarData());
+  return <Container>fdsflsdg</Container>;
+}
+
+export default CarList;
