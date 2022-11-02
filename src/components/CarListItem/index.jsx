d77a92be1e66826978b100bf88a575045c645efc
chaechae <dokailove6326@gmail.com>
2022-11-03 07:56:07 +0900

    Feat: Detail 정보 불러오기

--- src/components/CarListItem/index.jsx
+++ src/components/CarListItem/index.jsx
@@ -1,21 +1,24 @@
 import React from "react";
+import { Link } from "../../../node_modules/react-router-dom/dist/index";
 import { Container, Title, Info, Image } from "./CarListItem.styled";
 function CarListItem({ car }) {
   return (
-    <Container>
-      <div>
-        <Title>
-          {car.attribute.brand}
-          <br />
-          {car.attribute.name}
-        </Title>
-        <Info>
-          {car.attribute.segment} / {car.attribute.fuelType}
-          <br />월 {car.amount} 원 부터
-        </Info>
-      </div>
-      <Image src={car.attribute.imageUrl} alt={car.attribute.name}></Image>
-    </Container>
+    <Link to={`/detail/${car.id}`}>
+      <Container>
+        <div>
+          <Title>
+            {car.attribute.brand}
+            <br />
+            {car.attribute.name}
+          </Title>
+          <Info>
+            {car.attribute.segment} / {car.attribute.fuelType}
+            <br />월 {car.amount} 원 부터
+          </Info>
+        </div>
+        <Image src={car.attribute.imageUrl} alt={car.attribute.name}></Image>
+      </Container>
+    </Link>
   );
 }
 
--- src/pages/CarDetail/index.jsx
+++ src/pages/CarDetail/index.jsx
@@ -2,6 +2,9 @@ import React from "react";
 import Header from "@/components/Header/index";
 
 function CarDetail() {
+  // const { id } = useParams();
+  // const { carList } = useContext(CarListContext);
+  // const [carDetail] = carList.filter((item) => item.id === +id);
   return (
     <div>
       <Header title="차량상세" isBackBtn={true} />
