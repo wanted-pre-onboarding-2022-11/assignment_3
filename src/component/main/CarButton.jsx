9eb398c3c9706aa38ee8b5c0729a9bbc06adc39a
simyuseon <ysoen0508@naver.com>
2022-11-03 14:57:59 +0900

    Feat: 버튼 색 구현 및 메인 로딩 화면 추가

--- src/component/main/CarButton.jsx
+++ src/component/main/CarButton.jsx
@@ -1,55 +1,106 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
 import { carAPI } from "../../api/api";
 
-const CarButton = ({ setCars }) => {
+const CarButton = ({ setCars, setLoading }) => {
+  const [all, setAll] = useState(true);
+  const [E, setE] = useState(false);
+  const [D, setD] = useState(false);
+  const [C, setC] = useState(false);
   return (
     <ButtonDiv>
-      <Button
+      <button
+        className={all ? "selected-button" : "button"}
         onClick={() => {
-          carAPI.getAllCar().then((res) => setCars(res.payload));
+          setLoading(true);
+          setAll(true);
+          setE(false);
+          setD(false);
+          setC(false);
+          carAPI.getAllCar().then((res) => {
+            setLoading(false);
+            setCars(res.payload);
+          });
         }}
       >
         전체
-      </Button>
-      <Button
+      </button>
+      <button
+        className={E ? "selected-button" : "button"}
         onClick={() => {
-          carAPI.getECar().then((res) => setCars(res.payload));
+          setLoading(true);
+          setAll(false);
+          setE(true);
+          setD(false);
+          setC(false);
+          carAPI.getECar().then((res) => {
+            setLoading(false);
+            setCars(res.payload);
+          });
         }}
       >
         대형
-      </Button>
-      <Button
+      </button>
+      <button
+        className={D ? "selected-button" : "button"}
         onClick={() => {
-          carAPI.getDCar().then((res) => setCars(res.payload));
+          setLoading(true);
+          setAll(false);
+          setE(false);
+          setD(true);
+          setC(false);
+          carAPI.getDCar().then((res) => {
+            setLoading(false);
+            setCars(res.payload);
+          });
         }}
       >
         중형
-      </Button>
-      <Button
+      </button>
+      <button
+        className={C ? "selected-button" : "button"}
         onClick={() => {
-          carAPI.getCCar().then((res) => setCars(res.payload));
+          setLoading(true);
+          setAll(false);
+          setE(false);
+          setD(false);
+          setC(true);
+          carAPI.getCCar().then((res) => {
+            setLoading(false);
+            setCars(res.payload);
+          });
         }}
       >
         소형
-      </Button>
+      </button>
     </ButtonDiv>
   );
 };
 const ButtonDiv = styled.div`
   border-bottom: 1px solid black;
   padding: 6px 8px 6px 12px;
+  .button {
+    background-color: #d9d9d9;
+    color: black;
+    border-radius: 30px;
+    width: 62px;
+    height: 27px;
+    font-weight: 700;
+    font-size: 14px;
+    border: none;
+    margin-right: 6px;
+  }
+  .selected-button {
+    background-color: black;
+    color: white;
+    border-radius: 30px;
+    width: 62px;
+    height: 27px;
+    font-weight: 700;
+    font-size: 14px;
+    border: none;
+    margin-right: 6px;
+  }
 `;
 
-const Button = styled.button`
-  background-color: #d9d9d9;
-  color: black;
-  border-radius: 30px;
-  width: 62px;
-  height: 27px;
-  font-weight: 700;
-  font-size: 14px;
-  border: none;
-  margin-right: 6px;
-`;
 export default CarButton;
--- src/page/Main.jsx
+++ src/page/Main.jsx
@@ -4,23 +4,28 @@ import Car from "../component/main/Car";
 import None from "../component/main/None";
 import { useEffect, useState } from "react";
 import { carAPI } from "../api/api";
-
 import React from "react";
 import styled from "styled-components";
+import Loading from "../component/main/Loading";
+
 const Main = () => {
   const [cars, setCars] = useState();
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     carAPI.getAllCar().then((res) => {
       setCars(res.payload);
+      setLoading(false);
     });
   }, []);
 
   return (
     <Container>
       <Top />
-      <CarButton setCars={setCars} cars={cars} />
-      {cars?.length === 0 ? (
+      <CarButton setCars={setCars} cars={cars} setLoading={setLoading} />
+      {loading ? (
+        <Loading />
+      ) : cars?.length === 0 ? (
         <None />
       ) : (
         cars?.map((car) => {
