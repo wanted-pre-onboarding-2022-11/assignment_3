c72a3eb12f0ff599f870e39c42e2266765f23a80
0SCAR0421 <oscar0421@kakao.com>
2022-11-03 10:53:00 +0900

    Feat: CarItem, Notification 컴포넌트 매핑

--- src/components/index.jsx
+++ src/components/index.jsx
@@ -1,3 +1,5 @@
 export { default as Button } from "./Button";
 export { default as CarFilter } from "./CarFilter";
 export { default as Layout } from "./Layout";
+export { default as CarItem } from "./CarItem";
+export { default as Notification } from "./Notification";
