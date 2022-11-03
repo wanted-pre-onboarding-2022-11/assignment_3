5f47d49fc41bb7d8801fb7d4c29c64ef49aa80ef
chaechae66 <chaechae6326@gmail.com>
2022-11-03 18:53:26 +0900

    Feat: CarItem 컴포넌트 구현

---
+++ src/components/CarItem/index.jsx
@@ -0,0 +1,49 @@
+import React from "react";
+import styled from "styled-components";
+
+const CarItem = ({ car }) => {
+  return (
+    <Container>
+      <Box>
+        <div>
+          <b>{car.attribute.brand}</b>
+          <br />
+          <b>{car.attribute.name}</b>
+        </div>
+        <div>
+          <div>
+            {car.attribute.segment} / {car.attribute.fuelType}
+          </div>
+          <div>월 {car.amount} 원 부터</div>
+        </div>
+      </Box>
+      <Image src={car.attribute.imageUrl} />
+    </Container>
+  );
+};
+export default CarItem;
+
+const Container = styled.div`
+  width: 100%;
+  padding: 20px;
+  display: flex;
+  justify-content: space-between;
+`;
+
+const Image = styled.img`
+  width: 152px;
+  height: 80px;
+  background-color: #d9d9d9;
+`;
+
+const Box = styled.div`
+  > div > b {
+    font-size: 14px;
+  }
+  > div:first-child {
+    margin-bottom: 8px;
+  }
+  > div:last-child {
+    font-size: 12px;
+  }
+`;
--- src/pages/Detail/index.jsx
+++ src/pages/Detail/index.jsx
@@ -2,6 +2,10 @@ import React from "react";
 import Header from "../../components/Header/index";
 
 const Detail = () => {
+  // const { carDataList } = useCars();
+  // const { id } = useParams();
+  // const carData = carDataList.filter((e) => e.id === Number(id))[0];
+
   return (
     <>
       <Header title="상세차량" hasBack />
