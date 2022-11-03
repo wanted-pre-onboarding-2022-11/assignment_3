53a328580624b2c0c87319ce74a8b49c2ffe6b5f
0SCAR0421 <oscar0421@kakao.com>
2022-11-03 10:50:39 +0900

    Feat: CartItem 컴포넌트 작성

---
+++ src/components/CarItem/index.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+import { useNavigate } from "../../../node_modules/react-router-dom/dist/index";
+import { convertName } from "@utils/convertValue";
+import { CarItemContainer, LeftItem, RightItem } from "./CarItem.styled";
+import { convertFuel } from "../../utils/convertValue";
+
+const CarItem = ({ id, brand, name, segment, fuelType, imageUrl, amount }) => {
+  const navigate = useNavigate();
+  const handleTargetPage = (target) => {
+    navigate(`/detail/${target}`);
+  };
+
+  return (
+    <CarItemContainer onClick={() => handleTargetPage(id)}>
+      <LeftItem>
+        <div>
+          <p>{brand}</p>
+          <p>{name}</p>
+        </div>
+        <div>
+          <p>
+            <span>{convertName(segment)}</span>
+            <span> / </span>
+            <span>{convertFuel(fuelType)}</span>
+          </p>
+          <p>월 {amount} 원 부터</p>
+        </div>
+      </LeftItem>
+      <RightItem image={imageUrl} />
+    </CarItemContainer>
+  );
+};
+
+export default CarItem;
