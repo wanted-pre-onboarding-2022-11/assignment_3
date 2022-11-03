d815a32006423577f5e0927205c6344a8822740c
yong <tkddbs1644@gmail.com>
2022-11-03 13:03:24 +0900

    Feat: Layout 컴포넌트 구현

---
+++ src/components/Layout.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import styled from "styled-components";
+
+const Layout = ({ children }) => {
+  return <Container>{children}</Container>;
+};
+
+export default Layout;
+
+const Container = styled.div`
+  max-width: 450px;
+  min-width: 360px;
+  height: 100vh;
+  box-sizing: border-box;
+  margin: 0 auto;
+  background-color: #fafafa;
+  display: flex;
+  flex-direction: column;
+`;
