2860c0c18186b17d18d87971b4332afe3315b995
chaechae66 <chaechae6326@gmail.com>
2022-11-03 19:58:13 +0900

    Feat: 로딩, 차량이 없을 때 화면 구현

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -4,9 +4,10 @@ import Header from "../../components/Header/index";
 import { useCars } from "../../context/CarDataListContext";
 import { useSearchParams } from "../../../node_modules/react-router-dom/dist/index";
 import CarItem from "../../components/CarItem/index";
+import styled from "styled-components";
 
 const Home = () => {
-  const { carDataList } = useCars();
+  const { carDataList, isLoading } = useCars();
   const [searchParams] = useSearchParams();
   const fuelType = useMemo(() => searchParams.get("fuelType"), [searchParams]);
   const segment = useMemo(() => searchParams.get("segment"), [searchParams]);
@@ -29,6 +30,16 @@ const Home = () => {
     return false;
   });
 
+  if (isLoading) {
+    return (
+      <>
+        <Header title="전체차량" />
+        <TagList />
+        <Box>불러오는 중</Box>
+      </>
+    );
+  }
+
   if (!fuelType && !segment) {
     return (
       <>
@@ -37,6 +48,16 @@ const Home = () => {
         {carDataList.map((car) => (
           <CarItem key={car.id} car={car} />
         ))}
+      </>
+    );
+  }
+
+  if (filterdList.length === 0) {
+    return (
+      <>
+        <Header title="전체차량" />
+        <TagList />
+        <Box>차량이 없습니다.</Box>
       </>
     );
   }
@@ -53,3 +74,12 @@ const Home = () => {
 };
 
 export default Home;
+
+const Box = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 100%;
+  height: calc(100vh - 107px);
+  font-weight: 700;
+`;
