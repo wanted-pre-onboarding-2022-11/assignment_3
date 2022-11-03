98ed4c579deb843513af8e0bf28d209ff6a9afb0
yong <tkddbs1644@gmail.com>
2022-11-03 13:30:13 +0900

    Feat: Detail 페이지 리다이렉트 기능 구현

--- src/pages/Detail.jsx
+++ src/pages/Detail.jsx
@@ -1,14 +1,31 @@
 import React from "react";
 import Header from "../components/Header";
 import { useCar } from "@/contexts/carContext";
+import { useNavigate } from "react-router-dom";
+import { useEffect } from "react";
 
 const Detail = () => {
+  const navigate = useNavigate();
   const { car } = useCar();
-  //console.log(car);
+
+  useEffect(() => {
+    if (!car) {
+      navigate("/");
+    }
+  }, [car, navigate]);
+
+  if (!car) {
+    return;
+  }
+
   return (
     <>
       <Header title="차량상세" hasBack />
-      {car.attribute.name}
+      <img src={car.attribute.imageUrl} alt={car.attribute.name} />
+      <span>브랜드 : {car.attribute.brand}</span>
+      <div>{car.attribute.name}</div>
+      <div>{car.amount}</div>
+      <div>{car.segment}</div>
     </>
   );
 };
