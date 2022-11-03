b9ff08d8c69a272149e15a80f6dd9124808fa757
yong <tkddbs1644@gmail.com>
2022-11-03 13:02:46 +0900

    design: 글로벌 css 추가

---
+++ src/styles/constants.js
@@ -0,0 +1,11 @@
+export const COLOR_PALETTE = {
+  BLACK: "#000000",
+  GRAY: "#D9D9D9",
+  BLUE: "#0094FF",
+  BACKGROUND_HOVER: "#f0eded",
+};
+
+export const FONT_SIZE = {
+  SMALL: "0.8rem",
+  LARGE: "1.2rem",
+};
