c0c974f00f30c2a1a51fe45b8342f3c284e00151
0SCAR0421 <oscar0421@kakao.com>
2022-11-03 10:54:23 +0900

    Feat: CarDetail 컴포넌트 작성 및 디자인

--- src/pages/CarDetail/index.jsx
+++ src/pages/CarDetail/index.jsx
@@ -1,15 +1,66 @@
 import React, { useContext } from "react";
 import { CarDataListContext } from "@/contexts/CarDataListContext";
 import { useParams } from "react-router-dom";
+import { Notification } from "@components/index";
+import {
+  CarDetailContainer,
+  CarThumbnail,
+  CarHeader,
+  CarTitle,
+  CarContent,
+} from "./CarDetail.styled";
+import { convertFuel, convertName, convertDate } from "../../utils/convertValue";
 
 const CarDetail = () => {
   const { carDataList, isLoading } = useContext(CarDataListContext);
   const { id } = useParams();
   const carData = carDataList.filter((e) => e.id === Number(id))[0];
 
-  if (isLoading) return <div>로딩중...</div>;
+  if (isLoading) return <Notification>불러오는 중</Notification>;
 
-  return <div>{carData.attribute.name}</div>;
+  return (
+    <CarDetailContainer>
+      <CarThumbnail image={carData.attribute.imageUrl}></CarThumbnail>
+      <CarHeader>
+        <p>{carData.attribute.brand}</p>
+        <p>{carData.attribute.name}</p>
+        <p>{carData.amount}</p>
+      </CarHeader>
+      <CarTitle>차량 정보</CarTitle>
+      <CarContent>
+        <div>
+          <p>차종</p>
+          <p>{convertName(carData.attribute.segment)}</p>
+        </div>
+        <div>
+          <p>연료</p>
+          <p>{convertFuel(carData.attribute.fuelType)}</p>
+        </div>
+        <div>
+          <p>이용 가능일</p>
+          <p>{convertDate(carData.startDate)}</p>
+        </div>
+      </CarContent>
+      <CarTitle>보험</CarTitle>
+      <CarContent>
+        {carData.insurance.map((e) => (
+          <div key={e.name}>
+            <p>{e.name}</p>
+            <p>{e.description}</p>
+          </div>
+        ))}
+      </CarContent>
+      <CarTitle>추가상품</CarTitle>
+      <CarContent>
+        {carData.additionalProducts.map((e) => (
+          <div key={e.name}>
+            <p>{e.name}</p>
+            <p>{e.amount}</p>
+          </div>
+        ))}
+      </CarContent>
+    </CarDetailContainer>
+  );
 };
 
 export default CarDetail;
