a654855bc26de4c9894aef4e2f71d0e41588dd23
devyouth94 <devyouth94@gmail.com>
2022-11-03 13:06:21 +0900

    Design: css 설정 및 아이콘 추가

---
+++ src/styles/styled.js
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+const GSInfoText = styled.span`
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+
+  font-size: 17px;
+  font-weight: 700;
+`;
+
+export { GSInfoText };
