3e865ab62e01d4081ca1b0a7c45486159d88b698
0SCAR0421 <oscar0421@kakao.com>
2022-11-03 10:51:20 +0900

    Design: Layout 컴포넌트 디자인 수정

--- src/components/Layout/Layout.styled.jsx
+++ src/components/Layout/Layout.styled.jsx
@@ -1,9 +1,35 @@
 import styled from "styled-components";
 
+export const LayoutContainer = styled.div`
+  max-width: 450px;
+  margin: 0 auto;
+  min-height: 100vh;
+`;
+
+export const LayoutNavBar = styled.div`
+  position: relative;
+`;
+
+export const PrevButton = styled.div`
+  position: absolute;
+  top: 20px;
+  left: 18px;
+  cursor: pointer;
+`;
+
 export const LayoutHeader = styled.h1`
   display: flex;
-  color: #4e4e4e;
   justify-content: center;
-  font-size: 30px;
-  margin-top: 50px;
+  align-items: center;
+  height: 60px;
+  font-weight: 700;
+  font-size: 17px;
+  margin: 0;
+  border-bottom: 1px solid var(--black);
+`;
+
+export const LayoutContent = styled.div`
+  min-height: 75vh;
+  height: 100%;
+  background: rosybrown;
 `;
